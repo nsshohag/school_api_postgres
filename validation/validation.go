476f57c9d39f96863e79792911b719ad7ba9c591
Nazmus Sadat <nsshohag2011@gmail.com>
2025-03-23 12:35:05 +0600

    graceful shutdown and versioning added

--- validation/validation.go
+++ validation/validation.go
@@ -10,18 +10,40 @@ func ValidateStudent(s models.Student) error {
 	if s.Name == "" {
 		return errors.New("name is required")
 	}
-	if s.Age <= 0 || s.Age > 120 {
-		return errors.New("age must be a positive number and less than 120")
+
+	if s.Age == 0 {
+		return errors.New("provide age")
 	}
+
+	if s.Age < 0 || s.Age > 120 {
+
+		if s.Age < 0 {
+			return errors.New("age must be a positive number")
+		}
+		if s.Age > 120 {
+			return errors.New("age is too high")
+		}
+	}
+
+	if s.Class == 0 {
+		return errors.New("provide class")
+	}
+
 	if s.Class < 1 || s.Class > 10 {
-		return errors.New("class must be between 1 to 10")
+		if s.Class < 0 {
+			return errors.New("class must be a positive number")
+		}
+		if s.Class > 10 {
+			return errors.New("class is too high")
+		}
 	}
+
 	return nil
 }
 
 // validateAge checks if the age is valid
 func ValidateAge(age int) error {
-	if age <= 0 {
+	if age < 0 {
 		return errors.New("age must be a positive number")
 	}
 	if age > 120 {
